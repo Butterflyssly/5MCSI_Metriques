ffafec38ac06059a2168519c36baee16252cd4b8
Butterflyssly <chirah70@gmail.com>
2025-04-01 15:27:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,3 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from datetime import datetime
-from urllib.request import urlopen
-import sqlite3
-import requests
-                                                                                                                                       
-app = Flask(__name__) #commit                                                                                                                 
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
-
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
@@ -35,40 +21,6 @@ def mongraphique():
 @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
-
-@app.route('/commits/')
-def commits():
-    try:
-        url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.get(url, headers=headers)
-
-        if response.status_code != 200:
-            return jsonify({'error': 'Problème avec l\'API GitHub', 'status_code': response.status_code}), 500
-
-        commits_data = response.json()
-
-        
-        if not isinstance(commits_data, list):
-            return jsonify({'error': 'Format de réponse invalide'}), 500
-
-        
-        commit_counts = {}
-        for commit in commits_data:
-            date_str = commit['commit']['author']['date']
-            date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
-            minute = date_obj.strftime('%Y-%m-%d %H:%M')
-
-            if minute not in commit_counts:
-                commit_counts[minute] = 0
-            commit_counts[minute] += 1
-
-        results = [{'minute': minute, 'commit_count': count} for minute, count in commit_counts.items()]
-
-        return jsonify(results=results)
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
   app.run(debug=True)
