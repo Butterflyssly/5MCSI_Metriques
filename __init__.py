5f8cf575bd42c9ac5417b4f1c9c581176bd74446
Butterflyssly <chirah70@gmail.com>
2025-04-01 16:24:53 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,20 @@
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
+from datetime import datetime
+from urllib.request import urlopen
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__) #commit                                                                                                                 
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+  
+if __name__ == "__main__":
+  app.run(debug=True)
+
+@app.route("/contact/")
 def contact():
     return render_template("contact.html")
 
