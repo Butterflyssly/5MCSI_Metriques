bf2bc122ec1c64863778aa20602b660462d6f296
Butterflyssly <chirah70@gmail.com>
2025-04-01 15:47:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,5 +38,33 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
 
+@app.route('/commits/')
+def commits():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return jsonify({'error': 'Problème avec l\'API GitHub', 'status_code': response.status_code}), 500
+
+    commits_data = response.json()
+    commit_counts = {}
+
+    for commit in commits_data:
+        date_str = commit['commit']['author']['date']
+        date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        minute = date_obj.strftime('%Y-%m-%d %H:%M')  # Format : YYYY-MM-DD HH:MM
+        
+        if minute not in commit_counts:
+            commit_counts[minute] = 0
+        commit_counts[minute] += 1
+
+    results = [{'minute': minute, 'commit_count': count} for minute, count in commit_counts.items()]
+    return jsonify(results=results)
+
+@app.route('/commits_graph/')
+def commits_graph():
+    return render_template("commits.html")
+
 if __name__ == "__main__":
   app.run(debug=True)
